cb2dcd4b3ebe4dfac144d203c9056a7490615cac
Partition <groudon@freemail.gr>
2021-03-01 00:49:28 +0200

    Added -rngmap thing, changed .format to f string

--- commands/BaseCommands.py
+++ commands/BaseCommands.py
@@ -1,5 +1,9 @@
 from discord.ext.commands import Cog, command
-from random import randint
+from random import randint, choice
+from json import load
+from discord.embeds import Embed
+from discord import Colour
+from discord import File
 
 
 class BaseCommands(Cog, name="Base Commands"):
@@ -24,8 +28,25 @@ class BaseCommands(Cog, name="Base Commands"):
         """
         c = randint(0, 1)
         if c == 0:
-            result = "Heads"
+            result = "**Heads**"
         else:
-            result = "Tails"
+            result = "**Tails**"
 
-        await ctx.send("You flipped " + result)
+        await ctx.send(
+            embed=Embed(title="Coinflip 🪙", description=f"You flipped {result}", color=Colour.dark_purple()))
+
+    @command()
+    async def rngmap(self, ctx):
+        """
+        Picks a random map out of a preset map pool
+        """
+        with open("utils/rng_maps.json") as file:
+            maps = load(file)
+        random_map = choice(list(maps.keys()))
+
+        file = File(f"C:\Py PUG Bot\PUG-Bot\IDs\{maps[random_map]}.png", filename=f"{maps[random_map]}.png") #Reminder to change path here
+        embed = Embed(title="RNG Map",
+                      description=f"You will be playing [**{random_map}**](https://www.brawl.com/games/ctf/maps/{maps[random_map]}) ({maps[random_map]})", color=Colour.dark_purple())
+        embed.set_image(url=f"attachment://{maps[random_map]}.png")
+        await ctx.send(file=file, embed=embed)
+
