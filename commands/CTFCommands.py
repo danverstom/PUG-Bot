470de9daad4f737f1f0d51e46c81aed256dfc1df
ningeek212 <ningeek212@gmail.com>
2021-03-02 17:00:14 -0500

    Add findmap command

--- commands/CTFCommands.py
+++ commands/CTFCommands.py
@@ -1,16 +1,13 @@
 from discord.ext.commands import Cog, command
 from discord import File, Embed, Colour
 from utils.ctf_stats import get_server_games, CTFGame
-from utils.utils import response_embed
+from utils.utils import response_embed, create_list_pages
 from random import choice
 from json import load
-from math import ceil
-import asyncio
 
 # Slash commands support
 from discord_slash.cog_ext import cog_slash, manage_commands
 from utils.config import SLASH_COMMANDS_GUILDS
-from discord_slash import SlashContext
 
 
 class CTFCommands(Cog, name="CTF Commands"):
@@ -33,6 +30,29 @@ class CTFCommands(Cog, name="CTF Commands"):
                       color=Colour.dark_purple())
         embed.set_image(url=f"attachment://{maps[random_map]}.png")
         await ctx.send(file=file, embed=embed)
+
+    @cog_slash(name="findmap", description="Finds all maps in rotation that contains the input",
+               options=[manage_commands.create_option(name="search",
+                                                      description="The string to search with",
+                                                      option_type=3, required=False)], guild_ids=SLASH_COMMANDS_GUILDS)
+    async def findmap(self, ctx, search=""):
+        """
+        Finds all maps in rotation that contains the input
+        """
+        with open("utils/maps.json") as file:
+            maps = load(file)
+        if search:
+            list_maps = [(map_name, maps[map_name]) for map_name in list(maps.keys()) if
+                         search.lower() in map_name.lower()]
+        else:
+            list_maps = list(maps.items())
+
+        map_str = list()
+
+        for (map_name, map_id) in list_maps:
+            map_str.append(f"{map_name} ({map_id})")
+
+        await create_list_pages(self.bot, ctx, "Maps Found:", map_str)
 
     @cog_slash(name="stats", description="Gets most recent stats from match 1 and 2",
                guild_ids=SLASH_COMMANDS_GUILDS, options=[])
@@ -77,68 +97,3 @@ class CTFCommands(Cog, name="CTF Commands"):
             await response_embed(ctx, "No Games Found", "There are no match games in the past 10 games played.")
         else:
             await ctx.send(embed=embed)
-
-    @cog_slash(name="maplist", description="List of CTF current map rotation",
-               guild_ids=SLASH_COMMANDS_GUILDS, options=[])
-    async def maplist(self, ctx):
-
-        with open('utils/maps.json') as file:
-            maps = load(file)
-
-        contents = []
-        elmnts = 10  # Maps per page!!
-        pages = ceil(len(maps) / elmnts)
-        i = 0
-        page = ""
-        cur_page = 1
-        for map in maps:
-            i += 1
-            page = page + str(f'{map} ({maps[map]})\n')
-            if i == elmnts:
-                contents.append(page)
-                page = ""
-                i = 0
-        contents.append(page)
-
-        embed = Embed(title="Map List", description=contents[cur_page - 1], colour=Colour.dark_purple())
-        embed.set_footer(text=f"Page {cur_page}/{pages}")
-        message = await ctx.send(embed=embed)
-        # getting the message object for editing and reacting
-
-        await message.add_reaction("◀️")
-        await message.add_reaction("▶️")
-
-        def check(reaction, user):
-            return user == ctx.author and str(reaction.emoji) in ["◀️", "▶️"]
-            # This makes sure nobody except the command sender can interact with the "menu"
-
-        while True:
-            try:
-                reaction, user = await self.bot.wait_for("reaction_add", timeout=60, check=check)
-                # waiting for a reaction to be added - times out after x seconds, 60 in this
-                # example
-
-                if str(reaction.emoji) == "▶️" and cur_page != pages:
-                    cur_page += 1
-                    embed = Embed(title="Map List", description=contents[cur_page - 1],
-                                  colour=Colour.dark_purple())
-                    embed.set_footer(text=f"Page {cur_page}/{pages}")
-                    await message.edit(embed=embed)
-                    await message.remove_reaction(reaction, user)
-
-                elif str(reaction.emoji) == "◀️" and cur_page > 1:
-                    cur_page -= 1
-                    embed = Embed(title="Map List", description=contents[cur_page - 1],
-                                  colour=Colour.dark_purple())
-                    embed.set_footer(text=f"Page {cur_page}/{pages}")
-                    await message.edit(embed=embed)
-                    await message.remove_reaction(reaction, user)
-
-                else:
-                    await message.remove_reaction(reaction, user)
-                    # removes reactions if the user tries to go forward on the last page or
-                    # backwards on the first page
-            except asyncio.TimeoutError:
-                await message.delete()
-                break
-                # ending the loop if user doesn't react after x seconds
--- utils/utils.py
+++ utils/utils.py
@@ -1,4 +1,7 @@
 from discord import Embed, Colour
+from utils.config import ELEMENTS_PER_PAGE
+from math import ceil
+from asyncio import sleep as async_sleep, TimeoutError
 
 
 async def error_embed(ctx, description):
@@ -14,3 +17,53 @@ async def success_embed(ctx, description):
 async def response_embed(ctx, title, description):
     embed = Embed(title=title, description=description, color=Colour.dark_purple())
     await ctx.send(embed=embed)
+
+
+async def create_list_pages(bot, ctx, title, info):
+    contents = []
+    num_pages = ceil(len(info) / ELEMENTS_PER_PAGE)
+    page = ""
+    current_page = 1
+    for index, value in enumerate(info):
+        page = page + str(value + "\n")
+        if not (index + 1) % ELEMENTS_PER_PAGE:
+            contents.append(page)
+            page = ""
+    contents.append(page)
+
+    embed = Embed(title=title, description=contents[current_page - 1], colour=Colour.dark_purple())
+    embed.set_footer(text=f"Page {current_page}/{num_pages}")
+    message = await ctx.send(embed=embed)
+
+    await message.add_reaction("◀️")
+    await message.add_reaction("▶️")
+
+    def check(reaction, user):
+        return user == ctx.author and str(reaction.emoji) in ["◀️", "▶️"]
+
+    while True:
+        try:
+            reaction, user = await bot.wait_for("reaction_add", timeout=60, check=check)
+
+            if str(reaction.emoji) == "▶️" and current_page != num_pages:
+                current_page += 1
+                embed = Embed(title="Map List", description=contents[current_page - 1],
+                              colour=Colour.dark_purple())
+                embed.set_footer(text=f"Page {current_page}/{num_pages}")
+                await message.edit(embed=embed)
+                await message.remove_reaction(reaction, user)
+
+            elif str(reaction.emoji) == "◀️" and current_page > 1:
+                current_page -= 1
+                embed = Embed(title="Map List", description=contents[current_page - 1],
+                              colour=Colour.dark_purple())
+                embed.set_footer(text=f"Page {current_page}/{num_pages}")
+                await message.edit(embed=embed)
+                await message.remove_reaction(reaction, user)
+
+            else:
+                await message.remove_reaction(reaction, user)
+        except TimeoutError:
+            await message.edit(content="Message Expired", embed=None)
+            await message.clear_reactions()
+            break
