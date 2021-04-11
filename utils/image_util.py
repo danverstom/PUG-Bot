7bfe9ad05f663e3cbd46714ebacbb48fbe1ef236
mikeo <29011818+mikeo@users.noreply.github.com>
2021-04-11 23:57:37 +0100

    add compress function param type and default
    makes it easier for people. if u wanna pass quality in u can, but its 30 by default

--- utils/image_util.py
+++ utils/image_util.py
@@ -3,7 +3,7 @@ from PIL import Image
 
 
 # https://github.com/carzam87/python-bulk-image-optimizer/blob/master/bulk-image-optimizer.py
-def compress(location):
+def compress(location: str, quality: int = 30) -> Image:
     """Compress an image on the disk"""
     opt = Image.open(location) # dont handle error
     # Convert .pgn to .jpg
@@ -11,7 +11,7 @@ def compress(location):
         rgb_opt = opt.convert('RGB')
         rgb_opt.save(location)
         opt = Image.open(location)
-    opt.save(location, optimize=True, quality=85)
+    opt.save(location, optimize=True, quality=quality)
     opt.close()
     return opt
 
