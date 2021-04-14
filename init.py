c496666215bad73dcd57f68dfa531759c1d5c26d
Thomas Danvers <tomtomgaming101@gmail.com>
2021-04-14 14:59:46 +0100

    added hypercorn to serve web app

--- init.py
+++ init.py
@@ -1,7 +1,14 @@
 from bot import bot, bot_token
 from webserver.app import app
+from hypercorn.asyncio import serve
+from hypercorn.config import Config
 
-bot.loop.create_task(app.run_task())
+config = Config()
+config.bind = ["localhost:5000"]
+
+
+# bot.loop.create_task(app.run_task())
+bot.loop.create_task(serve(app, config=config))
 bot.run(bot_token)
 
 
