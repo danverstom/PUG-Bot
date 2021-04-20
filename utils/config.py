592db91209b9a9be1f7122a54d6083ddbc7ca697
Partition <groudon@freemail.gr>
2021-04-20 13:28:50 +0300

    added timestamp (local) to logging

--- utils/config.py
+++ utils/config.py
@@ -27,4 +27,4 @@ def get_debug_status():
     return debug
 
 
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', level=logging.INFO) #local time !
