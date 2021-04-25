6eba541524810d6c5199d839b20dcbc803228d67
Tom <tomtomgaming101@gmail.com>
2021-04-25 01:33:59 +0100

    fixed a large number of redboo blunders

--- commands/CTFCommands.py
+++ commands/CTFCommands.py
@@ -16,7 +16,6 @@ import aiohttp
 from asyncio import sleep as async_sleep
 import logging
 from utils.plot_utils import *
-from mojang import MojangAPI
 
 # ss
 import os
@@ -410,7 +409,11 @@ class CTFCommands(Cog, name="CTF Commands"):
         if ign:
             username = ign
         else:
-            username = Player.exists_discord_id(ctx.author.id)
+            player = Player.exists_discord_id(ctx.author.id)
+            if player:
+                username = player.minecraft_username
+            else:
+                username = None
         if not username:
             await error_embed(ctx, "Please input a player or `/register` to get your own stats")
             return
@@ -510,5 +513,5 @@ class CTFCommands(Cog, name="CTF Commands"):
         await discord_message.edit(content="Done! Sending results...")
         await ctx.send(file=chart_file)
         await create_list_pages(self.bot, ctx, info=class_stats_list, title=f"{mode.title()} stats | {username}",
-                                elements_per_page=1, thumbnails=[f"https://cravatar.eu/helmavatar/{MojangAPI.get_uuid(username)}/128.png"],
+                                elements_per_page=1, thumbnails=[f"https://cravatar.eu/helmavatar/{username}/128.png"],
                                 can_be_reversed=True)
